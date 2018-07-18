235219e140eeac8c486dfd75ee9693e0487381b2
Alex Shklianko <asl@enonic.com>
2018-07-18 23:39:37 +0300

    initial commit

---
+++ src/main/resources/site/lib/mustache.js
@@ -0,0 +1,26 @@
+/**
+ * Mustache template related functions.
+ *
+ * @example
+ * var mustacheLib = require('/lib/mustache');
+ *
+ * @module mustache
+ */
+
+var service = __.newBean('com.enonic.lib.mustache.MustacheService');
+
+/**
+ * This function renders a view using mustache.
+ *
+ * @example-ref examples/mustache/render.js
+ *
+ * @param view Location of the view. Use `resolve(..)` to resolve a view.
+ * @param {object} model Model that is passed to the view.
+ * @returns {string} The rendered output.
+ */
+exports.render = function (view, model) {
+    var processor = service.newProcessor();
+    processor.view = view;
+    processor.model = __.toScriptValue(model);
+    return processor.process();
+};
---
+++ src/test/resources/site/test/mustache-test.js
@@ -0,0 +1,28 @@
+var mustache = require('/lib/mustache');
+
+function assertHtmlEquals(res, actual) {
+    testInstance.assertHtmlEquals(resolve(res), actual);
+}
+
+exports.testRender = function () {
+    var view = resolve('view/test.html');
+    var result = mustache.render(view, {
+            fruits: [
+                {
+                    name: 'Apple',
+                    color: 'Red'
+                },
+                {
+                    name: 'Pear',
+                    color: 'Green'
+                }
+            ]
+        }
+    );
+
+    assertHtmlEquals('view/test-result.html', result);
+};
+
+exports.testExamples = function () {
+    testInstance.runScript('/site/lib/examples/mustache/render.js')
+};
